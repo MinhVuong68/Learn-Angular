802c66dd7cdf72902930b162d575c20313d57c3f
nguyenminhvuong <minhvuong06082001@gmail.com>
2023-02-20 20:24:18 +0700

    choose district with city

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,10 +15,31 @@ export class HomeComponent implements OnInit {
     { name: 'Nho', price: -10, discount: false },
     { name: 'Cam', price: -30, discount: false },
     { name: 'Buoi', price: 20, discount: true }
-
+  ]
+  districts: any = ['Quận huyện'];
+  vietnamData = [
+    { city: 'Chọn thành phố', district: ['Quận huyện'] },
+    { city: 'An Giang', district: ['Thành phố Long Xuyên','Thành phố Châu Đốc','Thị xã Tân Châu','Huyện An Phú','Huyện Châu Phú']},
+    { city: 'Bà Rịa - Vũng Tàu', district: ['Thành phố Vũng Tàu','Thị xã Bà Rịa','Thị xã Phú Mỹ','Huyện Châu Đức','Huyện Côn Đảo']},
+    { city: 'Long An', district: ['Thành phố Tân An','Huyện Cần Giuộc','Huyện Cần Đước','Huyện Đức Hòa','Huyện Đức Huệ']}
   ]
 
+
+  handleChaneCity(event: any): void {
+    const city = event.target.value;
+    // Solution 1
+    // const search = this.vietnamData.filter(data => data.city === city)
+    // if(search && search.length >0) { 
+    //   this.districts = search[0].district;
+    // }
+
+    //solution 2
+    this.districts = this.vietnamData.find(data => data.city === city)?.district || []
+
+
+  }
+
   ngOnInit(): void {
-    
+    console.log('cities:',this.vietnamData);
   }
 }
