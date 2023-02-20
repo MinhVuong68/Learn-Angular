3df31aac03463c8c8a8244f5f62f60babf1b1ccc
nguyenminhvuong <minhvuong06082001@gmail.com>
2023-02-20 17:49:38 +0700

    demo ngFor object value

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   
+  name = 'Vuong';
+  age = 25;
+  fruits = ['Táo','Nho','Cam','Sau rieng'];
+  fruits2 = [
+    { name: 'Táo', price: 20, discount: true},
+    { name: 'Nho', price: -10, discount: false },
+    { name: 'Cam', price: -30, discount: false },
+    { name: 'Buoi', price: 20, discount: true }
+
+  ]
+
+  ngOnInit(): void {
+    
+  }
 }
